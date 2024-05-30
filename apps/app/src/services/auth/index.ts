055d7fa93a648e02aa2ac381f67ff1b416b0c036
NestorMosqueraBmatch <129396602+NestorMosqueraBmatch@users.noreply.github.com>
2024-05-29 22:14:26 -0500

    chore: vitalit

--- apps/app/src/services/auth/index.ts
+++ apps/app/src/services/auth/index.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const loginApi = async(id: string) => {
-    const { data } = await axios.post("http://localhost:8000/api/v1/auth/login", { id }, {
+    const { data } = await axios.post("https://vitalut.onrender.com/api/v1/auth/login", { id }, {
         headers: {
             "api-key": "apkcoervo34nf34f3g34"
         }
